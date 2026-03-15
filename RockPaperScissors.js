81d92cebb87b9f9058fffee713b69f075d7ad8e4
Abrar Ur Alam <safee20142@gmail.com>
2026-03-15 14:04:42 +0600

    Create function to return player's choice

--- RockPaperScissors.js
+++ RockPaperScissors.js
@@ -3,9 +3,20 @@ FUNCTION getComputerChoice():
     RANDOMIZE choice among "Rock", "Paper" and "Scissors"
     RETURN chosen string
 */
-
 function getComputerChoice(){
     const choices = ["Rock", "Paper", "Scissors"];
     return choices[Math.floor(Math.random() * 3)];
 }
 // console.log(getComputerChoice());
+
+
+/*
+FUNCTION getHumanChoice():
+    DISPLAY prompt for user's choice
+    READ user choice
+    RETURN user choice string
+*/
+function getHumanChoice(){
+    return prompt("Type your choice [Rock, Paper or Scissors]:");
+}
+// console.log(getHumanChoice());
